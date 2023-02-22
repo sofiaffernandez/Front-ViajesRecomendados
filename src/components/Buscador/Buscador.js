5f9c097746c93c21d5df5d2a2d8242d632ce1203
jsalga1 <114351648+jsalga1@users.noreply.github.com>
2023-02-22 23:31:33 +0100

    buscador+CSS

--- src/components/Buscador/Buscador.js
+++ src/components/Buscador/Buscador.js
@@ -1,84 +1,77 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { TextField, Button } from "@mui/material";
-import "./Buscador.css";
-
-function Buscador() {
-const [lugar, setLugar] = useState("");
-const [categoria, setCategoria] = useState("");
-const [results, setResults] = useState([]);
-
-const handleSubmit = (event) => {
-    event.preventDefault();
-    axios
-    .get("/recomendacion/buscar", {
-        params: { lugar: lugar, categoria: categoria },
-    })
-    .then((response) => {
-        setResults(response.data);
-    })
-    .catch((error) => {
+    import { useState } from "react";
+    import { BsSearch } from "react-icons/bs";
+    import { Link } from "react-router-dom";
+    import '../Buscador/Buscador.css';
+    
+    // Obtenemos la variable de entorno que necesitamos
+    const { REACT_APP_BACKEND } = process.env;
+    // Definimos nuestro componente Buscador
+    const Buscador = () => {
+    // Definimos los estados que necesitamos para manejar la búsqueda y los resultados
+    const [categoria, setCategoria] = useState("");
+    const [lugar, setLugar] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+    const [order, setOrder] = useState("");
+    // Función para realizar la búsqueda en el backend
+    const buscar = async () => {
+        try {
+        // Creamos la URL con los parámetros de búsqueda
+        const url = `${REACT_APP_BACKEND}/recomendaciones?categoria=${categoria}&lugar=${lugar}&order=${order}`;
+        // Hacemos la petición GET al backend
+        const res = await fetch(url, { method: "GET" });
+        // Parseamos la respuesta como JSON
+        const data = await res.json();
+        // Actualizamos el estado de los resultados
+        setSearchResults(data.data);
+        console.log(data)
+        } catch (error) {
         console.log(error);
-    });
-};
-
-const handleSort = (event) => {
-    event.preventDefault();
-    axios
-    .get("/recomendacion/ordenar", { params: { sort: "votos" } })
-    .then((response) => {
-        setResults(response.data);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-};
-
-const handleDetail = (event, id) => {
-    event.preventDefault();
-    axios
-    .get("/recomendacion/detalle", { params: { id: id } })
-    .then((response) => {
-        // hacer algo con la respuesta
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-};
-
-return (
-    <div className="buscador-container">
-    <div className="search-fields">
-        <TextField
-        label="Lugar"
-        value={lugar}
-        onChange={(e) => setLugar(e.target.value)}
-        />
-        <TextField
-        label="Categoría"
-        value={categoria}
-        onChange={(e) => setCategoria(e.target.value)}
-        />
-    </div>
-    <Button type="submit" variant="contained" onClick={handleSubmit}>
-        Buscar
-    </Button>
-    <Button onClick={handleSort} variant="contained">
-        Ordenar por votos
-    </Button>
-
-    {results.map((result) => (
-        <div key={result.id}>
-        <h2>{result.titulo}</h2>
-        <p>{result.descripcion}</p>
-        <Button onClick={(event) => handleDetail(event, result.id)}>
-            Ver detalle
-        </Button>
+        }
+    };
+    // Funciones para actualizar los estados de la categoría, lugar y orden
+    const handleChangeCategoria = (event) => {
+        setCategoria(event.target.value);
+    };
+    const handleChangeLugar = (event) => {
+        setLugar(event.target.value);
+    };
+    const handleOrderChange = (event) => {
+        setOrder(event.target.value);
+    };
+    // Función para limpiar los resultados de la búsqueda
+    const clearSearch = () => {
+        setSearchResults([]);
+    };
+    return (
+        <div className="buscador-container">
+        <div className="buscador-inputs">
+            <input
+            className="buscador-input"
+            type="text"
+            value={lugar}
+            placeholder="Buscar lugar"
+            onChange={handleChangeLugar}
+            />
+            <button className="buscador-button" onClick={buscar}>
+            <BsSearch />
+            </button>
         </div>
-    ))}
-    </div>
-);
-}
-
-export default Buscador;
+        <div className="buscador-inputs">
+            <input
+            className="buscador-input"
+            type="text"
+            value={categoria}
+            placeholder="Buscar categoría"
+            onChange={handleChangeCategoria}
+            />
+            <select className="buscador-select" value={order} onChange={handleOrderChange}>
+            <option value="">Ordenar por votos</option>
+            <option value="mas">Más votados</option>
+            <option value="menos">Menos votados</option>
+            </select>
+        </div>
+        </div>
+    );
+    };
+    export default Buscador;
 
