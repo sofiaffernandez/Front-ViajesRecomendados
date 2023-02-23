afe4b04a1fa29cda544ce20b0c3d4dc7e0a6bf1a
jsalga1 <114351648+jsalga1@users.noreply.github.com>
2023-02-23 23:24:57 +0100

    buscador

--- src/components/Buscador/Buscador.js
+++ src/components/Buscador/Buscador.js
@@ -2,7 +2,6 @@
     import { BsSearch, BsTrash } from "react-icons/bs";
     import { Link } from "react-router-dom";
     import "./Buscador.css";
-    import "../../App.css"
 
     const { REACT_APP_BACKEND } = process.env;
 
@@ -11,97 +10,99 @@
     const [lugar, setLugar] = useState("");
     const [searchResults, setSearchResults] = useState([]);
     const [order, setOrder] = useState("");
+    const [showResults, setShowResults] = useState(false);
 
-    const handleChangeCategoria = (event) => {
-        setCategoria(event.target.value);
+    const handleSearch = async () => {
+        try {
+        const url = `${REACT_APP_BACKEND}/recomendaciones?categoria=${categoria}&lugar=${lugar}&order=${order}`;
+        const res = await fetch(url, { method: "GET" });
+        const data = await res.json();
+        setSearchResults(data.data);
+        setShowResults(true);
+        } catch (error) {
+        console.log(error);
+        }
     };
 
-    const handleChangeLugar = (event) => {
-        setLugar(event.target.value);
-    };
-
-    const handleOrderChange = (event) => {
-        setOrder(event.target.value);
+    const handleKeyPress = (event) => {
+        if (event.key === "Enter") {
+        handleSearch();
+        }
     };
 
     const handleClear = () => {
         setCategoria("");
         setLugar("");
         setSearchResults([]);
+        setShowResults(false);
     };
-
-    const buscar = async () => {
-        try {
-        const url = `${REACT_APP_BACKEND}/recomendaciones?categoria=${categoria}&lugar=${lugar}&order=${order}`;
-        const res = await fetch(url, { method: "GET" });
-        const data = await res.json();
-        setSearchResults(data.data);
-        console.log(data);
-        } catch (error) {
-        console.log(error);
-        }
-    };
-
 
     return (
         <div className="buscador-container">
-        <div className="buscador-inputs">
+        <div className="buscador-inputs-1">
             <input
             className="buscador-input"
             type="text"
             value={lugar}
             placeholder="Buscar lugar"
-            onChange={handleChangeLugar}
+            onChange={(event) => setLugar(event.target.value)}
+            onKeyPress={handleKeyPress}
             />
-            <input
-            className="buscador-input"
-            type="text"
-            value={categoria}
-            placeholder="Buscar categoría"
-            onChange={handleChangeCategoria}
-            />
-            <select
-            className="buscador-select"
-            value={order}
-            onChange={handleOrderChange}
-            >
-            <option value="">Ordenar por votos</option>
-            <option value="mas">Más votados</option>
-            <option value="menos">Menos votados</option>
-            </select>
-            <button className="buscador-button" onClick={buscar}>
+            <button className="buscador-button" onClick={handleSearch}>
             <BsSearch />
             </button>
             <button className="buscador-button" onClick={handleClear}>
             <BsTrash />
             </button>
         </div>
-        <ul className="buscador-resultados">
-            {searchResults.length > 0 ? (
-            searchResults.map((result) => {
-                if (
-                result.categoria === categoria ||
-                result.lugar === lugar ||
-                categoria === "" ||
-                lugar === ""
-                ) {
-                return (
-                    <li className="resultados" key={result.id}>
-                    <Link to={`/recomendacion/${result.id}/detalle`}>
-                        <h3>{result.titulo}</h3>
-                    </Link>
-                    <h4>📍{result.lugar}</h4>
-                    <h4>{result.categoria}</h4>
-                    </li>
-                );
-                }
-            })
-            ) : (
-            <p></p>
-            )}
-        </ul>
+        <div className="buscador-inputs-2">
+            <input
+            className="buscador-input"
+            type="text"
+            value={categoria}
+            placeholder="Buscar categoría"
+            onChange={(event) => setCategoria(event.target.value)}
+            onKeyPress={handleKeyPress}
+            />
+            <select
+            className="buscador-select"
+            value={order}
+            onChange={(event) => setOrder(event.target.value)}
+            >
+            <option value="">Ordenar por votos</option>
+            <option value="mas">Más votados</option>
+            <option value="menos">Menos votados</option>
+            </select>
         </div>
-    );
+        {showResults && (
+            <ul className="buscador-resultados">
+            {searchResults.length > 0 ? (
+                searchResults.map((result) => {
+                if (
+                    (result.categoria === categoria || categoria === "") &&
+                    (result.lugar.toLowerCase().includes(lugar.toLowerCase()) ||
+                    lugar === "")
+                ) {
+                    return (
+                    <li key={result.id}>
+                        <Link to={`/recomendacion/${result.id}/detalle`}>
+                        <h3>{result.titulo}</h3>
+                        </Link>
+                        <h4>📍{result.lugar}</h4>
+                        <h4>{result.categoria}</h4>
+                    </li>
+                    );
+                } else {
+                    return null;
+                }
+                })
+            ) : (
+                <p>No se encontraron resultados.</p>
+            )}
+            </ul>
+        )}
+        </div>
+        );
     };
 
     export default Buscador;
